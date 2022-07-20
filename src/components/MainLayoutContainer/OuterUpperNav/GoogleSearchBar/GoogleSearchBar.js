066fb1c3a7ea9c9eda2e66674dd391a87792ce00
John Nydam <jnydam@me.com>
2022-07-20 06:07:35 -0400

    Added icons and boilerplate for upper search bar

---
+++ src/components/MainLayoutContainer/OuterUpperNav/GoogleSearchBar/GoogleSearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const GoogleSearchBar = (props) => {
+
+    return (<div className={styles.googleSearchBarContainer}>
+        <span>This is the google search bar</span>
+    </div>)
+}
+
+export default GoogleSearchBar;
--- src/components/MainLayoutContainer/OuterUpperNav/OuterUpperNav.js
+++ src/components/MainLayoutContainer/OuterUpperNav/OuterUpperNav.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 import styles from './OuterUpperNav.module.css';
 
+import leftSideMenuGraphic from '../../../assets/upperrightsideicon.png';
+import mIconGraphic from '../../../assets/mainmicon.png';
+
 const OuterUpperNav = (props) => {
 
     return (<div className={styles.outerUpperNavContainer}>
-        <span>This is the outer upper nav</span>
+        <div className={styles.leftDividerContainer}>
+            <img className={styles.iconGraphicStyle} src={leftSideMenuGraphic}>
+            </img>
+            <img className={styles.iconGraphicStyle} src={mIconGraphic}>
+            </img>
+            <span style={{fontSize: '2.2rem'}}>Gmail</span>
+        </div>
+        <div className={styles.rightDividerContainer}>
+             
+        </div>
     </div>)
 }
 
