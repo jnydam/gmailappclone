cf33dfd9517a632af58f1db90d43ecf49f3b62d7
John Nydam <jnydam@me.com>
2022-07-20 07:06:35 -0400

    Populated more of the icons and the search bars

--- src/components/MainLayoutContainer/OuterUpperNav/GoogleSearchBar/GoogleSearchBar.js
+++ src/components/MainLayoutContainer/OuterUpperNav/GoogleSearchBar/GoogleSearchBar.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import styles from './GoogleSearchBar.module.css';
+
+import searchMailIconGraphic from '../../../../assets/searchmailicon.png';
+import filterMailIconGraphic from '../../../../assets/googlefiltericon.png';
+
 const GoogleSearchBar = (props) => {
 
     return (<div className={styles.googleSearchBarContainer}>
-        <span>This is the google search bar</span>
+        <div className={styles.searchIconImageContainer}>
+            <img style={{height: '2rem', width: '2rem'}} src={searchMailIconGraphic}></img>
+        </div>
+        <input className={styles.searchMailInputStyle} placeholder='Search mail'></input>
+        <div className={styles.filterIconImageContainer}>
+            <img style={{height: '2rem', width: '2rem'}} src={filterMailIconGraphic}></img>
+        </div>
     </div>)
 }
 
--- src/components/MainLayoutContainer/OuterUpperNav/OuterUpperNav.js
+++ src/components/MainLayoutContainer/OuterUpperNav/OuterUpperNav.js
@@ -4,6 +4,9 @@ import styles from './OuterUpperNav.module.css';
 
 import leftSideMenuGraphic from '../../../assets/upperrightsideicon.png';
 import mIconGraphic from '../../../assets/mainmicon.png';
+import GoogleSearchBar from './GoogleSearchBar/GoogleSearchBar';
+
+import questionIconGraphic from '../../../assets/questionicon.png';
 
 const OuterUpperNav = (props) => {
 
@@ -13,10 +16,25 @@ const OuterUpperNav = (props) => {
             </img>
             <img className={styles.iconGraphicStyle} src={mIconGraphic}>
             </img>
-            <span style={{fontSize: '2.2rem'}}>Gmail</span>
+            <span style={{
+                fontSize: '1.6rem',
+                marginLeft: '0.8rem'}}>Gmail</span>
+            <GoogleSearchBar></GoogleSearchBar>
         </div>
         <div className={styles.rightDividerContainer}>
-             
+            <div className={styles.rightSideImageDivider}>
+                <img style={{height: '2rem'}} src={questionIconGraphic}>
+                </img>
+            </div>
+            <div className={styles.rightSideImageDivider}>
+
+            </div>
+            <div className={styles.rightSideImageDivider}>
+
+            </div>
+            <div className={styles.rightSideImageDivider}>
+
+            </div>
         </div>
     </div>)
 }
