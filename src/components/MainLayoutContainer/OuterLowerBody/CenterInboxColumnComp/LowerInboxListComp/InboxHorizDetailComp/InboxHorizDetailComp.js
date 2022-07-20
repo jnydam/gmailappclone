300a7a5cadfa4eb84e77edfe0c1232f855f47d8d
John Nydam <jnydam@me.com>
2022-07-20 19:49:12 -0400

    Added more information to the inbox detail row components

--- src/components/MainLayoutContainer/OuterLowerBody/CenterInboxColumnComp/LowerInboxListComp/InboxHorizDetailComp/InboxHorizDetailComp.js
+++ src/components/MainLayoutContainer/OuterLowerBody/CenterInboxColumnComp/LowerInboxListComp/InboxHorizDetailComp/InboxHorizDetailComp.js
@@ -2,10 +2,40 @@ import React from 'react';
 
 import styles from './InboxHorizDetailComp.module.css';
 
+import greySquareGraphic from '../../../../../../assets/greysquareicon.png';
+import greyStarGraphic from '../../../../../../assets/highlightfirststar.png';
+import yellowPromotionGraphic from '../../../../../../assets/yellowpromotionicon.png';
+
 const InboxHorizDetailComp = (props) => {
 
     return (<div className={styles.inboxHorizDetailCompContainer}>
         <div className={styles.leftDividerContainer}>
+            <div style={{marginLeft: '0.83rem'}} className={styles.iconDividerContainer}>
+                <img className={styles.leftSideIconStyle} src={greySquareGraphic}>
+                </img>
+            </div>
+            <div className={styles.iconDividerContainer}>
+                <img className={styles.leftSideIconStyle} src={greyStarGraphic}>
+                </img>
+            </div>
+            <div className={styles.iconDividerContainer}>
+                <img className={styles.leftSideIconStyle} src={yellowPromotionGraphic}>
+                </img>
+            </div>
+            <span>{props.sender}</span>
+        </div>
+        <div className={styles.middleDividerContainer}>
+            <span>
+                {props.subject}
+            </span>
+            <span>
+                -
+            </span>
+            <span>
+                {props.description}
+            </span>
+        </div>
+        <div className={styles.rightDividerContainer}>
 
         </div>
     </div>)
