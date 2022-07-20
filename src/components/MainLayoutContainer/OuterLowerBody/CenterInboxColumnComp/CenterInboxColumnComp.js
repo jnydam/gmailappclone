a57cc49a5eb88b897bb051650d5eef78d8f1fcf8
John Nydam <jnydam@me.com>
2022-07-20 18:30:11 -0400

    Added the refresh icon container

--- src/components/MainLayoutContainer/OuterLowerBody/CenterInboxColumnComp/CenterInboxColumnComp.js
+++ src/components/MainLayoutContainer/OuterLowerBody/CenterInboxColumnComp/CenterInboxColumnComp.js
@@ -2,11 +2,33 @@ import React from 'react';
 
 import styles from './CenterInboxColumnComp.module.css';
 
+import greySquareGraphic from '../../../../assets/greysquareicon.png';
+import simpleDownArrowGraphic from '../../../../assets/simpledownarrowicon.png';
+import refreshIconGraphic from '../../../../assets/refreshicon.png';
+import threeDotIconGraphic from '../../../../assets/threedoticon.png';
+
+
 const CenterInboxColumnComp = (props) => {
 
     return (<div className={styles.centerInboxColumnCompContainer}>
         <div className={styles.innerUpperRefreshCenterInboxRowContainer}>
+            <div className={styles.leftSideIconContainer}>
+                <div style={{marginLeft: '0.3rem'}} className={styles.mainImageContainer}>
+                    <img className={styles.largeIconStyle} src={greySquareGraphic}>
+                    </img>
+                    <img className={styles.smallIconStyle} src={simpleDownArrowGraphic}>
+                    </img>
+                </div>
+                <div className={styles.mainImageContainerModified}>
+                    <img className={styles.largeIconStyle} src={refreshIconGraphic}></img>
+                </div>
+                <div className={styles.mainImageContainer}>
+                    <img className={styles.largeIconStyle} src={threeDotIconGraphic}></img>
+                </div>
+            </div>
+            <div className={styles.rightSideIconContainer}>
 
+            </div>
         </div>
     </div>)
 }
