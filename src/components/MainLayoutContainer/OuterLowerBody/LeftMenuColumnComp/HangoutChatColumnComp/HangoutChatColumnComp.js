0436debeb023bab0bf6749e3e15413a1ca4dfa90
John Nydam <jnydam@me.com>
2022-07-21 07:23:23 -0400

    Added the upper header icons

--- src/components/MainLayoutContainer/OuterLowerBody/LeftMenuColumnComp/HangoutChatColumnComp/HangoutChatColumnComp.js
+++ src/components/MainLayoutContainer/OuterLowerBody/LeftMenuColumnComp/HangoutChatColumnComp/HangoutChatColumnComp.js
@@ -2,14 +2,25 @@ import React from 'react';
 
 import styles from './HangoutChatColumnComp.module.css';
 
+import hangoutProfileGraphic from '../../../../../assets/profilehangoutpic.png';
+import simpleDownArrowGraphic from '../../../../../assets/simpledownarrowicon.png';
+
 const HangoutChatColumnComp = (props) => {
 
     return (<div className={styles.hangoutChatColumnCompContainer}>
         <div className={styles.hangoutTitleRowContainer}>
-            <span>Hangouts</span>
+            <span style={{marginLeft: '1rem'}}>Hangouts</span>
         </div>
         <div className={styles.sampleHangoutRowContainer}>
-
+            <img style={{height: '2.5rem', marginLeft: '1rem'}} src={hangoutProfileGraphic}>
+            </img>
+            <span>John</span>
+            <img style={{height: '1rem'}} src={simpleDownArrowGraphic}>
+            </img>
+        </div>
+        <div className={styles.bodyHangoutRowContainer}>
+            <span>No recent chats</span>
+            <span>Start a new one</span>
         </div>
     </div>)
 }
--- src/components/MainLayoutContainer/OuterUpperNav/OuterUpperNav.js
+++ src/components/MainLayoutContainer/OuterUpperNav/OuterUpperNav.js
@@ -7,6 +7,9 @@ import mIconGraphic from '../../../assets/mainmicon.png';
 import GoogleSearchBar from './GoogleSearchBar/GoogleSearchBar';
 
 import questionIconGraphic from '../../../assets/questionicon.png';
+import settingIconGraphic from '../../../assets/settingsicon.png';
+import nineGridIconGraphic from '../../../assets/ninegridicon.png';
+import upperRightProfileGraphic from '../../../assets/upperrightprofilepic.png';
 
 const OuterUpperNav = (props) => {
 
@@ -27,13 +30,16 @@ const OuterUpperNav = (props) => {
                 </img>
             </div>
             <div className={styles.rightSideImageDivider}>
-
+            <img style={{height: '2rem'}} src={settingIconGraphic}>
+                </img>
             </div>
             <div className={styles.rightSideImageDivider}>
-
+            <img style={{height: '2rem'}} src={nineGridIconGraphic}>
+                </img>
             </div>
-            <div className={styles.rightSideImageDivider}>
-
+            <div style={{marginRight: '0.9rem'}} className={styles.rightSideImageDivider}>
+            <img style={{height: '2.8rem'}} src={upperRightProfileGraphic}>
+                </img>
             </div>
         </div>
     </div>)
