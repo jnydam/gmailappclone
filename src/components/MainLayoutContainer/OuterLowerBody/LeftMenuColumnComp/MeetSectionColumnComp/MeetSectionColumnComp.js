8d44ecde780cb266030eb1064c6b93b3d3507373
John Nydam <jnydam@me.com>
2022-07-20 21:39:34 -0400

    Added more details to the meet tab

--- src/components/MainLayoutContainer/OuterLowerBody/LeftMenuColumnComp/MeetSectionColumnComp/MeetSectionColumnComp.js
+++ src/components/MainLayoutContainer/OuterLowerBody/LeftMenuColumnComp/MeetSectionColumnComp/MeetSectionColumnComp.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import MeetingHorizDetailSectionComp from './MeetingHorizDetailComp/MeetingHorizDetailComp';
 
 import styles from './MeetSectionColumnComp.module.css';
 
+import cameraGraphic from '../../../../../assets/meetcameraicon.png';
+import joinMeetingGraphic from '../../../../../assets/joinmeetingicon.png';
+
 const MeetSectionColumnComp = (props) => {
+
+    const meetingTabData = [
+        {
+            icon: cameraGraphic,
+            text: 'New Meeting'
+        },
+        {
+            icon: joinMeetingGraphic,
+            text: 'Join Meeting'
+        }
+    ]
 
     return (<div className={styles.meetSectionColumnCompContainer}>
         <div className={styles.meetTitleRowContainer}>
-
+            <span style={{marginLeft: '1rem'}}>Meet</span>
         </div>
+        {meetingTabData.map((meetingEntryData, index) => (
+            <MeetingHorizDetailSectionComp
+                key={index}
+                icon={meetingEntryData.icon}
+                text={meetingEntryData.text}></MeetingHorizDetailSectionComp>
+        ))}
     </div>)
 }
 
--- src/components/MainLayoutContainer/OuterLowerBody/LeftMenuColumnComp/MeetSectionColumnComp/MeetingHorizDetailComp/MeetingHorizDetailComp.js
+++ src/components/MainLayoutContainer/OuterLowerBody/LeftMenuColumnComp/MeetSectionColumnComp/MeetingHorizDetailComp/MeetingHorizDetailComp.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import styles from './MeetingHorizDetailComp.module.css';
+
 const MeetingHorizDetailSectionComp = (props) => {
 
-    return (<div className={styles.meetingHorizontalDetailSectionCompContainer}>
-        <span>This is the meeting horizontal section comp</span>
+    return (<div className={styles.meetingHorizontalDetailCompContainer}>
+        <div className={styles.imageContainer}>
+            <img className={styles.iconStyle} src={props.icon}>
+            </img>
+        </div>
+        <span>
+            {props.text}
+        </span>
     </div>)
 }
 
